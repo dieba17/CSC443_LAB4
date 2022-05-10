022b78bbfef90a754d4364363c55913e5aa19a5c
dieba17 <2020872786@student.uitm.edu.my>
2022-05-11 01:55:11 +0800

    handle_form

--- handle_form.php
+++ handle_form.php
@@ -17,6 +17,17 @@ if(!(strlen($_POST["email"]) >0)){
 $_POST["email"]=null;
 echo '<p><b> You Forgot to Enter Your Emails! </b></p>';
 }
+if(isset($_POST["gender"])){
+    if($_POST["gender"] == 'M'){
+        $message = '<b><p>Good Day, Sir </b></p>';
+    }
+    if($_POST["gender"] == 'F'){
+        $message = '<b><p>Good Day, Madam </b></p>';
+    }
+}else{
+    $_POST["gender"] = null;
+    echo '<p><b> You Forgot to Choose Your Gender! </b></p>';
+}
 ?>
 </body>
 </html>
