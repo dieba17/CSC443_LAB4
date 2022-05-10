7f0564cabc8cd921a96c26f3f856449eed4b465e
dieba17 <2020872786@student.uitm.edu.my>
2022-05-11 01:37:46 +0800

    handle_form

--- handle_form.php
+++ handle_form.php
@@ -5,13 +5,17 @@
 <body>
 <?php #Script handle form
 
-if (strlen($_POST["name"]) >0){
-    $_POST["name"] = $_POST["name"];
+if (strlen($_POST["comments"]) >0){
+    $_POST["comments"] = $_POST["comments"];
 }
 else{
-    $_POST["name"]=null;
+    $_POST["comments"]=null;
 
     echo '<p><b> You Forgot to Enter Your Name!</b></p>';
+}
+if(!(strlen($_POST["email"]) >0)){
+$_POST["email"]=null;
+echo '<p><b> You Forgot to Enter Your Emails! </b></p>';
 }
 ?>
 </body>
